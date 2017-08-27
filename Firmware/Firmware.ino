f9700b3d82148b374422de7f84a7a296bcbccd1c
Daniel Perez <danperez27@gmail.com>
2017-08-27 12:50:32 -0600

    Refactor operations commands and deleted analogic operations

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -7,178 +7,159 @@
 
 #define VALUE_INPUT 0
 #define VALUE_OUTPUT 1
-#define VALUE_HIGH 2
-#define VALUE_LOW 3
+#define VALUE_HIGH 0
+#define VALUE_LOW 1
 
 #define RESP_ERROR 3
 #define RESP_SUCCESS 2
 #define RESP_ON 1
 #define RESP_OFF 0
 
-#define COMMAND_TERMINATOR 59 //;
-#define COMMAND_START 36 //$
-
-#define HIGH_BIT_PART 1
-#define LOW_BIT_PART 0
+#define COMMAND_TERMINATOR 0x7e //~
+#define COMMAND_START 0x7f      //DEL
 
 boolean commandComplete = false;
 boolean startCopy = false;
-char command [3];
+char command[2];
 int commandOffset = 0;
 
-int parseBinaryStringToInt(char * s){
-    char* currentOffset = &s[0];
-    int result = 0;
-    while(*currentOffset){
-        result <<= 1;
-        if(*currentOffset++ == '1')
-            result ^= 1;
-    }
-    return (byte)result;
-}
-
-int parseCommandValue(char c, int bitOffset){
-    char s[4];
-    int i, x = 0;
-    int offSet = bitOffset == HIGH_BIT_PART ? 7 : 3;
-    int limit = bitOffset == HIGH_BIT_PART ? 4 : 0;
-    for(i = offSet; limit <= i; i--){
-        s[x] = ((c >>i)&0x01) == 0x01 ? '1' : '0';
-        x++;
-    }
-    int result = parseBinaryStringToInt(s);
-    return result;
-}
-
-void getIncomingBytes(){
+void getIncomingBytes()
+{
     char inChar = Serial.read();
-    if(inChar == COMMAND_TERMINATOR){
+    if (inChar == COMMAND_TERMINATOR)
+    {
         commandComplete = true;
         startCopy = false;
         commandOffset = 0;
-    } else if(startCopy) {
+    }
+    else if (startCopy)
+    {
         command[commandOffset] = inChar;
         commandOffset++;
-    }else if(inChar == COMMAND_START && !startCopy){
-      startCopy = true;
-      commandOffset = 0;
+    }
+    else if (inChar == COMMAND_START && !startCopy)
+    {
+        startCopy = true;
+        commandOffset = 0;
     }
 }
 
-void resetValues(){
+void resetValues()
+{
     commandComplete = false;
     startCopy = false;
     command[0] = '0';
     command[1] = '0';
-    command[2] = '0';
 }
 
-int getCommandValue(){
+int getPinNumber()
+{
     return (byte)command[1];
 }
 
-int getPinNumber(){
-    return (byte)command[2];
- 
+int getOperationType()
+{
+    return command[0] & 0x0f;
 }
 
-int getOperationType(){
-    return parseCommandValue(command[0], LOW_BIT_PART);
+int getOperation()
+{
+    return command[0] >> 4;
 }
 
-int getOperation(){
-    return parseCommandValue(command[0],HIGH_BIT_PART);
-}
-
-void setMode(){
-    int value = getCommandValue();
+void setMode()
+{
+    int value = getOperationType();
     int pin = getPinNumber();
-    if(value == VALUE_INPUT){
-        pinMode(pin,INPUT);
+    if (value == VALUE_INPUT)
+    {
+        pinMode(pin, INPUT);
         Serial.write(RESP_SUCCESS);
-    } else if(value == VALUE_OUTPUT){
-        pinMode(pin,OUTPUT);
+    }
+    else if (value == VALUE_OUTPUT)
+    {
+        pinMode(pin, OUTPUT);
         Serial.write(RESP_SUCCESS);
-    }else{
+    }
+    else
+    {
         Serial.write(RESP_ERROR);
     }
 }
 
-void setValue(){
-    int opType = getOperationType();
-    int value = getCommandValue();
+void setValue()
+{
+    int value = getOperationType();
     int pin = getPinNumber();
-    if(opType == DIGITAL){
-        if(value == VALUE_HIGH){
-            digitalWrite(pin, HIGH);
-        }else if(value == VALUE_LOW){
-            digitalWrite(pin, LOW);
-        }else {
-            Serial.write(RESP_ERROR);
-            return;
-        }
-        Serial.write(RESP_SUCCESS);
-    }else if(opType == ANALOG){
-        analogWrite(pin, value);
-        Serial.write(RESP_SUCCESS);
-    }else{
+    if (value == VALUE_HIGH)
+    {
+        digitalWrite(pin, HIGH);
+    }
+    else if (value == VALUE_LOW)
+    {
+        digitalWrite(pin, LOW);
+    }
+    else
+    {
+        Serial.write(RESP_ERROR);
+        return;
+    }
+    Serial.write(RESP_SUCCESS);
+}
+
+void getValue()
+{
+    int pin = getPinNumber();
+    if (digitalRead(pin) == HIGH)
+    {
+        Serial.write(RESP_ON);
+    }
+    else if (digitalRead(pin) == LOW)
+    {
+        Serial.write((byte)RESP_OFF);
+    }
+    else
+    {
         Serial.write(RESP_ERROR);
     }
 }
 
-void getValue(){
-    int opType = getOperationType();
-    int pin = getPinNumber();
-    int value = 0;
-    if(opType == DIGITAL){
-        value = digitalRead(pin);
-        if(value == HIGH){
-            Serial.write(RESP_ON);
-        }else if(value == LOW){
-            Serial.write((byte)RESP_OFF);
-        }else {
-            Serial.write(RESP_ERROR);
-        }
-    }else if(opType == ANALOG){
-        value = analogRead(pin);
-        Serial.write(value);
-    }else{
+void processCommand()
+{
+    int op = getOperation();
+    switch (op)
+    {
+    case OP_SETMODE:
+        setMode();
+        break;
+    case OP_SETVALUE:
+        setValue();
+        break;
+    case OP_GETVALUE:
+        getValue();
+        break;
+    default:
         Serial.write(RESP_ERROR);
-    }
-}
-
-void processCommand(){
-    int op  = getOperation();
-    switch(op){
-        case OP_SETMODE:
-            setMode();
-        break;
-        case OP_SETVALUE:
-            setValue();
-        break;
-        case OP_GETVALUE:
-            getValue();
-        break;
-        default:
-            Serial.write(RESP_ERROR);
     }
     resetValues();
-   // Serial.flush();
 }
 
-void setup() {
-  Serial.begin(9600);
-  Serial.flush();
+void setup()
+{
+    Serial.begin(9600);
+    Serial.flush();
 }
 
-void loop() {
-    if(Serial.available() > 0){
+void loop()
+{
+    if (Serial.available() > 0)
+    {
         getIncomingBytes();
         Serial.flush();
     }
 
-    if(commandComplete == true){
+    if (commandComplete == true)
+    {
         processCommand();
     }
-//Serial.println("loop");
 }
