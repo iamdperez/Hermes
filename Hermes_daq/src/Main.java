01f78bc36174bda1b063b3966633821e322dfd61
Daniel Perez <danperez27@gmail.com>
2017-08-30 07:42:34 -0600

    ADD methods pinMode and setValue to serialCommunication class

--- Hermes_daq/src/Main.java
+++ Hermes_daq/src/Main.java
@@ -1,5 +1,19 @@
+import serialCommunication.Commands;
+import serialCommunication.SerialCommunication;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.print("Hello world");
+        SerialCommunication sc = new SerialCommunication();
+        try {
+            System.out.println("Hello world: " + sc.getArduinoModel());
+            sc.pinMode(Commands.PIN_MODE_OUTPUT, 7);
+            sc.setValue(Commands.SET_VALUE_HIGH, 7);
+            Thread.sleep(2000);
+            sc.setValue(Commands.SET_VALUE_LOW, 7);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- Hermes_daq/src/serialCommunication/SerialCommunication.java
+++ Hermes_daq/src/serialCommunication/SerialCommunication.java
@@ -5,41 +5,47 @@ import com.fazecast.jSerialComm.SerialPort;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Arrays;
 
 public class SerialCommunication {
     private OutputStream _outputStream;
-    private InputStream _inputStram;
+    private InputStream _inputStream;
     private SerialPort _serialPort;
 
     public SerialCommunication(){
         _serialPort =  SerialPort.getCommPorts()[0];
         _serialPort.openPort();
-        _serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING,
-                100, 0);
+        _serialPort.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING,0, 0);
         _outputStream = _serialPort.getOutputStream();
-        _inputStram = _serialPort.getInputStream();
+        _inputStream = _serialPort.getInputStream();
     }
 
-    private byte[] sendCommand(byte command, int pinNumber) throws IOException {
+    private byte[] sendCommand(byte command, int pinNumber) throws IOException, InterruptedException {
         byte[] message = new byte[]{
                 Commands.COMMAND_HEADER,
                 command, (byte)pinNumber,
                 Commands.COMMAND_TRAILER };
         _outputStream.write(message);
-        int incomingAvailable = _inputStram.available();
+        int incomingAvailable = _inputStream.available();
+        while (incomingAvailable == 0){
+            Thread.sleep(20);
+            incomingAvailable = _inputStream.available();
+        }
         byte [] response = new byte[incomingAvailable];
-        _inputStram.read(response);
+        _inputStream.read(response);
         return response;
     }
 
-    public String getArduinoModel() throws IOException {
+    public String getArduinoModel() throws IOException, InterruptedException {
         byte [] model = sendCommand(Commands.GET_ARDUINO_MODEL, 0);
-        String modelBoard = "";
-        for (byte b:model) {
-            modelBoard += (char)b;
-        }
-        return modelBoard;
+        return new String(model);
+    }
+
+    public void pinMode(byte pinModeType, int pinNumber) throws IOException, InterruptedException {
+        byte [] result = sendCommand(pinModeType, pinNumber);
+    }
+
+    public void setValue(byte valueType, int pinNumber) throws IOException, InterruptedException {
+        byte [] result = sendCommand(valueType, pinNumber);
     }
 
     public boolean close(){
@@ -48,7 +54,8 @@ public class SerialCommunication {
 
     public void finalize(){
         _outputStream = null;
-        _inputStram = null;
+        _inputStream = null;
         _serialPort = null;
+        close();
     }
 }
