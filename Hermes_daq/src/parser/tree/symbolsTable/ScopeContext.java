a10dbf890bb8b82eee1eb13dfdc44fdb26d174fa
Daniel Perez <danperez27@gmail.com>
2017-10-23 16:57:00 -0600

    addded symbols table

---
+++ Hermes_daq/src/parser/tree/symbolsTable/ScopeContext.java
@@ -0,0 +1,40 @@
+package parser.tree.symbolsTable;
+
+import parser.tree.Types.Type;
+import parser.tree.Values.Value;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScopeContext {
+    private final Map<String, Value> values;
+    private final Map<String, Type> variables;
+
+    public ScopeContext(){
+        values = new HashMap<>();
+        variables = new HashMap<>();
+    }
+
+    public void declare(String variableName, Type type){
+        variables.put(variableName, type);
+        values.put(variableName,type.getDefaultValue());
+    }
+
+    public Type getType(String variableName){
+        return variables.get(variableName);
+    }
+
+    public boolean exist(String variableName){
+        return variables.containsKey(variableName);
+    }
+
+    public void setValue(String variableName, Value value){
+        values.replace(variableName, value);
+    }
+
+    public Value getValue(String variableName){
+        return values.get(variableName);
+    }
+
+
+}
---
+++ Hermes_daq/src/parser/tree/symbolsTable/SymbolsTable.java
@@ -0,0 +1,75 @@
+package parser.tree.symbolsTable;
+
+import parser.exeptions.SemanticException;
+import parser.tree.Types.Type;
+import parser.tree.Values.Value;
+
+import java.util.ArrayList;
+
+public class SymbolsTable {
+    private static SymbolsTable instance;
+    private final ArrayList<ScopeContext> contexts;
+    private SymbolsTable(){
+        contexts = new ArrayList<>();
+    }
+    public static synchronized SymbolsTable getInstance(){
+        if (instance == null)
+            return new SymbolsTable();
+        return instance;
+    }
+
+    public boolean exist(String variableName){
+        for(int i = contexts.size() - 1; i >= 0; i--){
+            if(contexts.get(i).exist(variableName))
+                return true;
+        }
+        return false;
+    }
+
+    public Value getValue(String variableName) throws SemanticException {
+        for(int i = contexts.size() - 1; i >= 0; i--){
+            if(contexts.get(i).exist(variableName))
+                return contexts.get(i).getValue(variableName);
+        }
+        throw new SemanticException("Not found variable `"+variableName+"`");
+    }
+
+    public void pushNewContext(){
+        contexts.add(new ScopeContext());
+    }
+
+    public ScopeContext popContext(){
+        if(contexts.size()==0)
+            return null;
+        ScopeContext sc = contexts.get(contexts.size()-1);
+        contexts.remove(contexts.size() - 1);
+        return sc;
+    }
+
+    private ScopeContext getCurrentContext(){
+        return contexts.get(contexts.size() - 1);
+    }
+
+    public void declare(String variableName, Type type){
+        ScopeContext sc = getCurrentContext();
+        sc.declare(variableName,type);
+    }
+
+    public Type getType(String variableName) throws SemanticException {
+        for(int i = contexts.size() - 1; i >= 0; i--){
+            if(contexts.get(i).exist(variableName))
+                return contexts.get(i).getType(variableName);
+        }
+        throw new SemanticException("Not found variable `"+variableName+"`");
+    }
+
+    public void setValue(String variableName, Value value){
+        for (int i = contexts.size() - 1; i >= 0; i--)
+        {
+            if (contexts.get(i).exist(variableName)){
+                contexts.get(i).setValue(variableName,value);
+                break;
+            }
+        }
+    }
+}
