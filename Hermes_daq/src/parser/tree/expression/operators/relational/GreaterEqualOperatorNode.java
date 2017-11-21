3ec520a752946be94ba7aa7241dd54d8ebda1892
Daniel Perez <danperez27@gmail.com>
2017-11-20 20:37:01 -0600

    evaluatign semantic and interpret code for relational operators

--- Hermes_daq/src/parser/tree/expression/operators/relational/GreaterEqualOperatorNode.java
+++ Hermes_daq/src/parser/tree/expression/operators/relational/GreaterEqualOperatorNode.java
@@ -1,7 +1,10 @@
 package parser.tree.expression.operators.relational;
 
+import parser.ParserUtils;
+import parser.exeptions.SemanticException;
 import parser.tree.Location;
 import parser.tree.types.Type;
+import parser.tree.values.IntValue;
 import parser.tree.values.Value;
 import parser.tree.expression.ExpressionNode;
 import parser.tree.expression.operators.BinaryOperator;
@@ -12,12 +15,30 @@ public class GreaterEqualOperatorNode extends BinaryOperator {
     }
 
     @Override
-    public Value interpret() {
-        return null;
+    public Value interpret() throws SemanticException {
+        Value left = getLeftNode().interpret();
+        Value right = getRightNode().interpret();
+        int iLeft = (int)left.getValue();
+        int iRight = (int)right.getValue();
+        int value = iLeft >= iRight  ? 1 : 0;
+        return new IntValue(value);
     }
 
     @Override
-    public Type evaluateSemantic() {
-        return null;
+    public Type evaluateSemantic() throws SemanticException {
+        Type left = getLeftNode().evaluateSemantic();
+        Type right = getRightNode().evaluateSemantic();
+
+        validateIdNode(getRightNode());
+        validateIdNode(getLeftNode());
+
+        if(operationIsValid(left,right))
+            return ParserUtils.intType;
+        String errorMessage = getSemanticErrorMessage("Greater Equal operator", left, right);
+        throw new SemanticException(errorMessage);
+    }
+
+    private boolean operationIsValid(Type left, Type right){
+        return (isIntType(left)&&isIntType(right));
     }
 }
