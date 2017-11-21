74c08dd3a2a8f92e6cbc5d8b725b60aa456ee166
Daniel Perez <danperez27@gmail.com>
2017-11-21 10:09:32 -0600

    Evaluate semantic and Interpret code in Ternary operator

--- Hermes_daq/src/parser/tree/expression/operators/TernaryOperatorNode.java
+++ Hermes_daq/src/parser/tree/expression/operators/TernaryOperatorNode.java
@@ -1,6 +1,11 @@
 package parser.tree.expression.operators;
 
+import parser.ParserUtils;
+import parser.exeptions.SemanticException;
 import parser.tree.Location;
+import parser.tree.types.IntType;
+import parser.tree.types.PinType;
+import parser.tree.types.SetType;
 import parser.tree.types.Type;
 import parser.tree.values.Value;
 import parser.tree.expression.ExpressionNode;
@@ -18,13 +23,41 @@ public class TernaryOperatorNode extends ExpressionNode {
     }
 
     @Override
-    public Value interpret() {
-        return null;
+    public Value interpret() throws SemanticException {
+        Value trueValue = getTruePart().interpret();
+        Value falseValue = getFalsePart().interpret();
+        Value condition = getCondition().interpret();
+        return (int)condition.getValue() > 0 ? trueValue.clone() : falseValue.clone();
     }
 
     @Override
-    public Type evaluateSemantic() {
-        return null;
+    public Type evaluateSemantic() throws SemanticException {
+        Type condition = getCondition().evaluateSemantic();
+        if(!typeIsValid(condition)){
+            throw new SemanticException(
+                    ParserUtils.getInstance().getLineErrorMessage("Condition must be a IntType not "
+                            +condition+".",getLocation())
+            );
+        }
+        Type falseExp = getFalsePart().evaluateSemantic();
+        Type trueExp = getTruePart().evaluateSemantic();
+
+        validateIdNode(getFalsePart());
+        validateIdNode(getTruePart());
+
+        if(falseExp.getClass() != trueExp.getClass()){
+            throw new SemanticException(
+                    ParserUtils.getInstance().getLineErrorMessage("Return value must be equal between "
+                            +trueExp+" and "+falseExp+".",getLocation())
+            );
+        }
+        return trueExp;
+    }
+
+    boolean typeIsValid(Type t){
+        return t instanceof IntType
+                || t instanceof PinType
+                || t instanceof SetType;
     }
 
     public ExpressionNode getTruePart() {
