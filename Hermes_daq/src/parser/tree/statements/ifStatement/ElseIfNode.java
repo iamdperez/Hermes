9e1132bb296cc0d9a976de9b6365415380ea9363
Daniel Perez <danperez27@gmail.com>
2017-11-22 11:02:26 -0600

    Validated semantic and interpret code in if statement

--- Hermes_daq/src/parser/tree/statements/ifStatement/ElseIfNode.java
+++ Hermes_daq/src/parser/tree/statements/ifStatement/ElseIfNode.java
@@ -1,24 +1,53 @@
 package parser.tree.statements.ifStatement;
 
+import parser.ParserUtils;
+import parser.exeptions.SemanticException;
 import parser.tree.Location;
 import parser.tree.expression.ExpressionNode;
 import parser.tree.statements.StatementNode;
+import parser.tree.types.IntType;
+import parser.tree.types.PinType;
+import parser.tree.types.SetType;
+import parser.tree.types.Type;
 
 import java.util.ArrayList;
 
-public class ElseIfNode {
-    private final Location location;
+public class ElseIfNode extends StatementNode {
     private final ExpressionNode condition;
     private final ArrayList<StatementNode> statementList;
 
     public ElseIfNode(Location location, ExpressionNode condition, ArrayList<StatementNode> statementList){
-        this.location = location;
+        super(location);
         this.condition = condition;
         this.statementList = statementList;
     }
 
-    public Location getLocation() {
-        return location;
+    @Override
+    public void validateSemantic() throws SemanticException {
+        Type conditionT = getCondition().evaluateSemantic();
+        if(!(typeIsValid(conditionT))){
+            throw new SemanticException(
+                    ParserUtils.getInstance()
+                            .getLineErrorMessage("Condition expression must be `IntType, SetType, PinType` not `"
+                                    +conditionT+"`. " ,condition.getLocation())
+            );
+        }
+        for (StatementNode item: statementList) {
+            item.validateSemantic();
+        }
+    }
+
+    @Override
+    public void interpret() throws SemanticException {
+        for (StatementNode item: statementList) {
+            item.interpret();
+        }
+    }
+
+    private boolean typeIsValid(Type t) {
+        return t instanceof IntType
+                || t instanceof PinType
+                || t instanceof SetType;
     }
 
     public ExpressionNode getCondition() {
--- Hermes_daq/src/parser/tree/statements/ifStatement/ElseNode.java
+++ Hermes_daq/src/parser/tree/statements/ifStatement/ElseNode.java
@@ -1,24 +1,35 @@
 package parser.tree.statements.ifStatement;
 
+import parser.exeptions.SemanticException;
 import parser.tree.Location;
 import parser.tree.statements.StatementNode;
 
 import java.util.ArrayList;
 
-public class ElseNode {
-    private final Location location;
+public class ElseNode extends StatementNode {
     private final ArrayList<StatementNode> statementList;
 
     public ElseNode(Location location, ArrayList<StatementNode> statementList){
-        this.location = location;
+        super(location);
         this.statementList = statementList;
-    }
-
-    public Location getLocation() {
-        return location;
     }
 
     public ArrayList<StatementNode> getStatementList() {
         return statementList;
+    }
+
+    @Override
+    public void validateSemantic() throws SemanticException {
+        if(getStatementList()!=null)
+            for (StatementNode item: statementList ) {
+                item.validateSemantic();
+            }
+    }
+
+    @Override
+    public void interpret() throws SemanticException {
+        for (StatementNode item: statementList ) {
+            item.interpret();
+        }
     }
 }
