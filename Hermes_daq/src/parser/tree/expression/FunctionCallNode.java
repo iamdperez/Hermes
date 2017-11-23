036e1abe27145bbfb9d87dd94fccc9b16a338d8e
Daniel Perez <danperez27@gmail.com>
2017-11-22 19:13:17 -0600

    function call node validate semantic and interpret code

--- Hermes_daq/src/parser/tree/expression/FunctionCallNode.java
+++ Hermes_daq/src/parser/tree/expression/FunctionCallNode.java
@@ -1,39 +1,118 @@
 package parser.tree.expression;
 
+import parser.ParserUtils;
+import parser.exeptions.SemanticException;
 import parser.tree.Location;
+import parser.tree.statements.StatementNode;
+import parser.tree.symbolsTable.FunctionSymbol;
+import parser.tree.symbolsTable.OverloadedFunction;
+import parser.tree.symbolsTable.SymbolsTable;
+import parser.tree.symbolsTable.VarSymbol;
+import parser.tree.types.IntType;
 import parser.tree.types.Type;
+import parser.tree.values.IntValue;
 import parser.tree.values.Value;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FunctionCallNode extends ExpressionNode {
-    private final IdNode name;
+    private final IdNode functionName;
     private final ArrayList<ExpressionNode> argumentList;
 
     public FunctionCallNode(Location location, IdNode name) {
         super(location);
-        this.name = name;
+        this.functionName = name;
         argumentList = null;
     }
 
     public FunctionCallNode(Location location, IdNode name, ArrayList<ExpressionNode> argumentList){
         super(location);
-        this.name = name;
+        this.functionName = name;
         this.argumentList = argumentList;
     }
 
     @Override
-    public Value interpret() {
-        return null;
+    public Value interpret() throws SemanticException {
+        FunctionSymbol fs = (FunctionSymbol)SymbolsTable.getInstance().getVariable(functionName.getName());
+        SymbolsTable.getInstance().pushNewContext();
+        OverloadedFunction of;
+        if(argumentList !=null){
+            of = getOverloadedFunctions(argumentList.size()).get(0);
+            of.getParams().forEach( o -> SymbolsTable.getInstance().declareVariable(o.getName(), new VarSymbol()));
+            for(int i = 0; i < argumentList.size(); i++){
+                ExpressionNode item = argumentList.get(i);
+                IntValue paramValue = (IntValue) item.interpret();
+                SymbolsTable.getInstance().setVariableValue(of.getParams().get(i).getName(),paramValue);
+            }
+        }else{
+            of = getOverloadedFunctions(0).get(0);
+        }
+        for(StatementNode item: of.getStatements()){
+            item.interpret();
+            /*TODO: al encontrar un return hacer break*/
+        }
+        SymbolsTable.getInstance().popContext();
+        return fs.getValue();
     }
 
     @Override
-    public Type evaluateSemantic() {
-        return null;
+    public Type evaluateSemantic() throws SemanticException {
+        if(!SymbolsTable.getInstance().variableExist(functionName.getName())){
+            throw new SemanticException(
+                    ParserUtils.getInstance().getLineErrorMessage(
+                            "Function `"+ functionName.getName()+"` must be declared before. ",getLocation())
+            );
+        }
+        if(paramsAreValid())
+            return SymbolsTable.getInstance().getVariableType(functionName.getName());
+
+        throw new SemanticException(
+                ParserUtils.getInstance().getLineErrorMessage(
+                        "Function `"+functionName.getName()+"` must send the correct params. ",getLocation())
+        );
     }
 
-    public IdNode getName() {
-        return name;
+    private boolean paramsAreValid() throws SemanticException {
+        if(argumentList == null){
+            List<OverloadedFunction> ofl = getOverloadedFunctions(0);
+            return ofl.size() == 1;
+        }
+
+        List<OverloadedFunction> ofl = getOverloadedFunctions(argumentList.size());
+        if(ofl.size() > 1)
+            return false;
+
+        for (ExpressionNode item: argumentList) {
+            Type t = item.evaluateSemantic();
+            if(!(t instanceof IntType)){
+                throw new SemanticException(
+                        ParserUtils.getInstance()
+                                .getLineErrorMessage(
+                                        "Param in function `"+functionName.getName()
+                                                +"` must be IntType not `"+t+"`. ",item.getLocation())
+                );
+            }
+
+        }
+         return true;
+    }
+
+    private List<OverloadedFunction> getOverloadedFunctions(int paramsCount) throws SemanticException {
+        FunctionSymbol fs = (FunctionSymbol)SymbolsTable.getInstance().getVariable(functionName.getName());
+
+        if(paramsCount == 0){
+            return fs.getOverloadedFunctions().stream()
+                    .filter(o -> o.getParams() == null).collect(Collectors.toList());
+        }
+        return fs.getOverloadedFunctions().stream()
+                .filter( o -> o.getParams()!= null && o.getParams().size() == paramsCount)
+                .collect(Collectors.toList());
+    }
+
+    public IdNode getFunctionName() {
+        return functionName;
     }
 
     public ArrayList<ExpressionNode> getArgumentList() {
